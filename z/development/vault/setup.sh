0e3340762bea41924dd20ac4229227cb2d743b1c
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-06-08 19:24:35 -0500

    update : hashicorp setup tested.

--- z/development/vault/setup.sh
+++ z/development/vault/setup.sh
@@ -2,9 +2,10 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 helm repo update
 
 helm install vault hashicorp/vault -f values.yaml
+helm upgrade vault hashicorp/vault -f values.yaml
 
-#helm uninstall vault
-#kubectl delete pvc -l app.kubernetes.io/name=vault
+helm uninstall vault
+kubectl delete pvc -l app.kubernetes.io/name=vault
 
 
 
@@ -16,24 +17,18 @@ kubectl exec -it vault-0 -- vault operator unseal <key-3>
 
 kubectl exec -it vault-0 -- vault login
 
-kubectl exec -it vault-0 -- vault secrets enable transit
-kubectl exec -it vault-0 -- vault auth enable jwt
-
 kubectl exec -it vault-0 -- vault operator raft list-peers
 kubectl exec -it vault-0 -- vault operator raft join http://vault-1.vault-internal:8200
 kubectl exec -it vault-0 -- vault operator raft join http://vault-2.vault-internal:8200
 
 
-kubectl exec vault-1 -- vault operator unseal <key1>
-kubectl exec vault-1 -- vault operator unseal <key2>
-kubectl exec vault-1 -- vault operator unseal <key3>
-
-kubectl exec vault-2 -- vault operator unseal <key1>
-kubectl exec vault-2 -- vault operator unseal <key2>
-kubectl exec vault-2 -- vault operator unseal <key3>
+kubectl exec -it vault-1 -- vault operator unseal <key1>
+kubectl exec -it vault-1 -- vault operator unseal <key2>
+kubectl exec -it vault-1 -- vault operator unseal <key3>
 
 
-
+kubectl exec -it vault-0 -- vault secrets enable transit
+kubectl exec -it vault-0 -- vault auth enable jwt
 
 kubectl cp jwt-config.json vault-0:/tmp/jwt-config.json
 kubectl exec -it vault-0 -- sh -c 'vault write auth/jwt/config @/tmp/jwt-config.json'
