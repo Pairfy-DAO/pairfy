9906f2da7d29fba05a14c873da0bcdf7b8aed9f8
Juan Rey <sudo.delete.evil@gmail.com>
2025-07-19 16:27:02 -0500

    deleted : base64 decoding from seller/userAuth. @common

--- common/src/sellerAuth.ts
+++ common/src/sellerAuth.ts
@@ -26,7 +26,7 @@ export const sellerMiddleware = (
   }
 
   try {
-    const privateKey = Buffer.from(process.env.AGENT_JWT_KEY!, 'base64').toString('utf-8');
+    const privateKey = process.env.AGENT_JWT_KEY as string
 
     const sessionData = jwt.verify(
       req.session.jwt,
