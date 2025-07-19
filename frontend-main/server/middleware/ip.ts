f7eea7b1407d627de9e267be5c8fde715f552548
Juan Rey <sudo.delete.evil@gmail.com>
2025-07-19 14:40:43 -0500

    testing deployment : ip middleware nuxt

--- frontend-main/server/middleware/ip.ts
+++ frontend-main/server/middleware/ip.ts
@@ -3,9 +3,14 @@ import { defineEventHandler, getHeader } from 'h3'
 export default defineEventHandler((event) => {
   const ip =
     getHeader(event, 'cf-connecting-ip') ||
-    getHeader(event, 'x-forwarded-for')?.split(',')[0]?.trim() || 
-    event.node.req.socket?.remoteAddress || 
-    '0.0.0.0' 
+    getHeader(event, 'x-forwarded-for')?.split(',')[0]?.trim() ||
+    event.node.req.socket?.remoteAddress ||
+    '0.0.0.0'
 
+
+  console.log(getHeader(event, 'cf-connecting-ip'))
+  console.log(getHeader(event, 'x-forwarded-for')?.split(',')[0]?.trim())
+  console.log(event.node.req.socket?.remoteAddress)
+  
   event.context.clientIP = ip
 })
