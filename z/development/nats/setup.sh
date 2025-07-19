37c90629fbf1c78e6704d69db579b7a0a5c69724
Juan Rey <sudo.delete.evil@gmail.com>
2025-07-19 18:19:11 -0500

    update : nats values.yaml

--- z/development/nats/setup.sh
+++ z/development/nats/setup.sh
@@ -1,3 +1,6 @@
+helm repo add nats https://nats-io.github.io/k8s/helm/charts/
+helm repo update
+
 helm install nats nats/nats --set=config.jetstream.enabled=true --set=config.cluster.enabled=true -f values.yaml
 
 #helm uninstall nats nats/nats
