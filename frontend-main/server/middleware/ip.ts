15d04a103d4caeb771994e4447e4420d216f72fd
Juan Rey <sudo.delete.evil@gmail.com>
2025-07-19 15:12:37 -0500

    debug - client ip.

--- frontend-main/server/middleware/ip.ts
+++ frontend-main/server/middleware/ip.ts
@@ -1,16 +1,37 @@
-import { defineEventHandler, getHeader } from 'h3'
+import { defineEventHandler, getHeader, createError } from 'h3'
 
 export default defineEventHandler((event) => {
-  const ip =
-    getHeader(event, 'cf-connecting-ip') ||
-    getHeader(event, 'x-forwarded-for')?.split(',')[0]?.trim() ||
-    event.node.req.socket?.remoteAddress ||
-    '0.0.0.0'
+  const req = event.node.req
 
+  const isDev =
+    process.env.NODE_ENV === 'development' ||
+    req.headers.host?.startsWith('localhost') ||
+    req.socket?.remoteAddress === '::1' || // IPv6 localhost
+    req.socket?.remoteAddress === '127.0.0.1' // IPv4 localhost
 
-  console.log(getHeader(event, 'cf-connecting-ip'))
-  console.log(getHeader(event, 'x-forwarded-for')?.split(',')[0]?.trim())
-  console.log(event.node.req.socket?.remoteAddress)
-  
-  event.context.clientIP = ip
+  if (isDev) {
+    event.context.clientIP =
+      getHeader(event, 'x-forwarded-for')?.split(',')[0]?.trim() ||
+      req.socket?.remoteAddress ||
+      '0.0.0.0'
+    return
+  }
+
+  const cfIP = getHeader(event, 'cf-connecting-ip')
+  const cfRay = getHeader(event, 'cf-ray')
+
+  if (!cfIP || !cfRay) {
+    console.warn('🚫 Cloudflare block', {
+      event: 'cloudflare',
+      ip: req.socket?.remoteAddress,
+      headers: req.headers,
+    })
+
+    throw createError({
+      statusCode: 403,
+      statusMessage: 'Forbidden: Request did not come through Cloudflare',
+    })
+  }
+
+  event.context.clientIP = cfIP
 })
