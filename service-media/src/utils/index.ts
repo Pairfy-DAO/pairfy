eff1de77f6ae339017ea718bc90bd61b316be782
Juan Rey <sudo.delete.evil@gmail.com>
2025-07-20 09:40:00 -0500

    update : service-media manifests.

--- service-media/src/utils/index.ts
+++ service-media/src/utils/index.ts
@@ -1,14 +1,5 @@
 import { logger } from "@pairfy/common";
 
-export const errorEvents: string[] = [
-  "exit",
-  "SIGINT",
-  "SIGTERM",
-  "SIGQUIT",
-  "uncaughtException",
-  "unhandledRejection",
-];
-
 export const catchError = (error?: any) => {
   logger.error(`EXIT=>${error}`);
 
