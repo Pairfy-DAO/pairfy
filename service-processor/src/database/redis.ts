8606adb82c2bf6cfde2c4ed656d6c84377f8cf03
Juan Rey <sudo.delete.evil@gmail.com>
2025-07-20 14:23:57 -0500

    Update redis.ts

--- service-processor/src/database/redis.ts
+++ service-processor/src/database/redis.ts
@@ -1,7 +1,7 @@
 import type { RedisOptions } from 'ioredis';
 
 export const connection: RedisOptions = {
-  host: process.env.REDIS_HOST as string,
+  host: process.env.REDIS_PROCESSOR_HOST as string,
   port: 6379,
   maxRetriesPerRequest: null,
 };
